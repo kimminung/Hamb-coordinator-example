67f137a18180cc864675ea432741e1c783825279
KimMinUng <minogo1054@paran.com>
2025-05-25 17:17:38 +0900

    Initial Commit

---
+++ HambTestTests/HambTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  HambTestTests.swift
+//  HambTestTests
+//
+//  Created by coulson on 5/25/25.
+//
+
+import Testing
+@testable import HambTest
+
+struct HambTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
